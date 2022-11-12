31bcdd3c1d74223af77f54357b61136812bf7097
mateusaires1 <airesmateus@hotmail.com>
2022-11-11 21:28:55 -0300

    commit Front GS MS

---
+++ src/test/java/fiap/microservico/Interface/InterfaceGS/InterfaceGsApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.microservico.Interface.InterfaceGS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceGsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
